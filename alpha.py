f224c17b006c645408b37ca670eab5e14c83b9e6
vinushaF <112194872+vinushaF@users.noreply.github.com>
2024-01-26 19:52:11 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,11 @@
+a = input("enter the alphabet in capital")
+if( a=="A"):
+    print("apple")
+elif(a=="B"):
+    print("banana")
+elif(a=="C"):
+    print("coconut")
+else:
+    print("the aplhabet is not valid")
+        
+
